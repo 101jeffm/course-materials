21f54deea543c5c454b8dfeba99dcd0fc6c15cd9
Jeffrey Michaelis <47127711+101jeffm@users.noreply.github.com>
2022-04-02 21:02:22 -0600

    Add files via upload

--- materials/lab/6/main/main.go
+++ materials/lab/6/main/main.go
@@ -9,6 +9,8 @@ import (
 	"scrape/scrape"
 )
 
+var LOG_LEVEL = 1
+
 //TODO_1: Logging right now just happens, create a global constant integer LOG_LEVEL 
 //TODO_1: When LOG_LEVEL = 0 DO NOT LOG anything
 //TODO_1: When LOG_LEVEL = 1 LOG API details only 
@@ -16,10 +18,14 @@ import (
 
 func main() {
 	
-	log.Println("starting API server")
+	if LOG_LEVEL == 1 || LOG_LEVEL == 2 {
+		log.Println("starting API server")
+	}
 	//create a new router
 	router := mux.NewRouter()
-	log.Println("creating routes")
+	if LOG_LEVEL == 1 || LOG_LEVEL == 2 {
+		log.Println("creating routes")
+	}
 	//specify endpoints
 	router.HandleFunc("/", scrape.MainPage).Methods("GET")
 
@@ -27,9 +33,9 @@ func main() {
 
 	router.HandleFunc("/indexer", scrape.IndexFiles).Methods("GET")
 	router.HandleFunc("/search", scrape.FindFile).Methods("GET")		
-    //TODO_2 router.HandleFunc("/addsearch/{regex}", scrape.TODOREPLACE).Methods("GET")
-    //TODO_3 router.HandleFunc("/clear", scrape.TODOREPLACE).Methods("GET")
-    //TODO_4 router.HandleFunc("/reset", scrape.TODOREPLACE).Methods("GET")
+    router.HandleFunc("/addsearch/{regex}", scrape.AddSearch).Methods("GET")
+    router.HandleFunc("/clear", scrape.Clear).Methods("GET")
+	router.HandleFunc("/reset", scrape.Reset).Methods("GET")
 
 
 
