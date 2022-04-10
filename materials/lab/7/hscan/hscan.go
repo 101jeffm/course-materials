cf234ec6c925fdbd467c4d336e2ae3122bf6ecc9
Jeffrey Michaelis <47127711+101jeffm@users.noreply.github.com>
2022-04-09 23:29:18 -0600

    Add files via upload

--- materials/lab/7/hscan/hscan.go
+++ materials/lab/7/hscan/hscan.go
@@ -8,14 +8,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 //==========================================================================\\
 
-var shalookup map[string]string
-var md5lookup map[string]string
+var shalookup = make(map[string]string)
+var md5lookup = make(map[string]string)
+var mutexSHA sync.Mutex
+var mutexMD5 sync.Mutex
 
-func GuessSingle(sourceHash string, filename string) {
+func GuessSingle(sourceHash string, filename string) string {
+
+	hashLen := len(sourceHash)
 
 	f, err := os.Open(filename)
 	if err != nil {
@@ -31,24 +36,29 @@ func GuessSingle(sourceHash string, filename string) {
 		// TODO - From the length of the hash you should know which one of these to check ...
 		// add a check and logicial structure
 
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (MD5): %s\n", password)
-		}
-
-		hash = fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+		if hashLen == 32 {
+			hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (MD5): %s\n", password)
+				return password
+			}
+		} else if hashLen == 64 {
+			hash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+				return password
+			}
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
 	}
+	return ""
 }
 
-func GenHashMaps(filename string) {
 
+func GenHashMaps(filename string) {
 	//TODO
 	//itterate through a file (look in the guessSingle function above)
 	//rather than check for equality add each hash:passwd entry to a map SHA and MD5 where the key = hash and the value = password
@@ -59,6 +69,52 @@ func GenHashMaps(filename string) {
 	//Test and record the time it takes to scan to generate these Maps
 	// 1. With and without using go subroutines
 	// 2. Compute the time per password (hint the number of passwords for each file is listed on the site...)
+
+	var routines sync.WaitGroup
+	
+	f, err := os.Open(filename)
+	g, err1 := os.Open(filename)
+	if err != nil || err1 != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+	defer g.Close()
+
+	routines.Add(1)
+	go StoreSHAHash(f, &routines)
+	routines.Add(1)
+	go StoreMD5Hash(g, &routines)
+	routines.Wait()
+}
+
+func StoreMD5Hash(file *os.File, routines *sync.WaitGroup){
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		password := scanner.Text()
+		mutexMD5.Lock()
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+		md5lookup[hash] = password
+		mutexMD5.Unlock()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+	routines.Done()
+}
+
+func StoreSHAHash(file *os.File, routines *sync.WaitGroup){
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		password := scanner.Text()
+		mutexSHA.Lock()
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+		shalookup[hash] = password
+		mutexSHA.Unlock()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+	routines.Done()
 }
 
 func GetSHA(hash string) (string, error) {
@@ -75,5 +131,13 @@ func GetSHA(hash string) (string, error) {
 
 //TODO
 func GetMD5(hash string) (string, error) {
-	return "", errors.New("not implemented")
+	password, ok := md5lookup[hash]
+	if ok {
+		return password, nil
+
+	} else {
+
+		return "", errors.New("password does not exist")
+
+	}
 }
--- materials/lab/7/hscan/hscan_test.go
+++ materials/lab/7/hscan/hscan_test.go
@@ -7,10 +7,24 @@ import (
 )
 
 func TestGuessSingle(t *testing.T) {
-	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f") // Currently function returns only number of open ports
-	want := "foo"
+	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f","/home/cabox/workspace/course-materials/materials/lab/7/main/wordlist.txt") // Currently function returns only number of open ports
+	want := "Nickelback4life"
 	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+		t.Errorf("got %s, wanted %s", got, want)
 	}
+}
 
+func TestGenHashMaps(t *testing.T){
+	GenHashMaps("/home/cabox/workspace/course-materials/materials/lab/7/main/Top304Thousand-probable-v2.txt")
+}
+
+func TestDrMikePwd(t *testing.T) {
+	GenHashMaps("/home/cabox/workspace/course-materials/materials/lab/7/main/Top304Thousand-probable-v2.txt")
+	got1, _ := GetMD5("90f2c9c53f66540e67349e0ab83d8cd0")
+	want1 := "p@ssword"
+	got2,_ := GetSHA("1c8bfe8f801d79745c4631d09fff36c82aa37fc4cce4fc946683d7b336b63032")
+	want2 := "letmein"
+	if got1 != want1 || got2 != want2 {
+		t.Errorf("got1 %s, wanted1 %s\ngot2 %s, wanted2 %s", got1, want1, got2, want2)
+	}
 }
